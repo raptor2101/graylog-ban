f70f477e271841d8e3209733bc990b2c64afdc4e
raptor2101 <raptor2101@mykolab.com>
2022-03-04 08:42:00 +0100

    Handly empty results

--- graylog/ip_report.py
+++ graylog/ip_report.py
@@ -1,3 +1,6 @@
+import syslog
+
+
 class IpReport:
     def __init__(self, query, result):
 
@@ -5,6 +8,11 @@ class IpReport:
         if "errors" not in result:
             result = result["results"][query["id"]]["search_types"][query["search_types"][0]["id"]]
             self.name = result["name"]
+        else:
+            return
+        if "rows" in result:
             for row in result["rows"]:
                 if len(row["key"]) == 1:
                     self.ipAddresses.add(row["key"][0])
+        else:
+            syslog.syslog(syslog.LOG_INFO, "No results emitted by query %s." % self.name)
