bab5ba1bbe2f387e4051aec28df8986b06e3dc9b
raptor2101 <mail@raptor2101.de>
2025-06-29 11:15:11 +0200

    add logging end expose verifyServerCert

--- graylog/api.py
+++ graylog/api.py
@@ -1,23 +1,24 @@
 import requests
 import json
-import syslog
-
 
 class Api:
-    def __init__(self, server, token):
+    def __init__(self, server, token, verify_server_sert, logger):
         self.url = f"{server}/api/views/search/sync"
         self.auth_token = (token, "token")
         self.headers = {"Content-Type": "application/json", "X-Requested-By": "jupyter"}
+        self.logger = logger
+        self.verifyServerCert = verify_server_sert
 
     def query(self, query):
         payload = {
             "queries": [query]
         }
 
-        response = requests.post(self.url, data=json.dumps(payload), auth=self.auth_token, headers=self.headers)
+        response = requests.post(self.url, data=json.dumps(payload), auth=self.auth_token, headers=self.headers, verify=self.verifyServerCert)
         if response.status_code == 200:
             return response.json()
         elif response.status_code == 400:
-            syslog.syslog(syslog.LOG_ERR, "Unable to process query. ErrorMessage: %s." % response.content)
+            self.logger.error("Unable to process query. ErrorMessage: %s." % response.content)
         else:
-            syslog.syslog(syslog.LOG_ERR, "Unable to process query. StatusCode: %d." % response.status_code)
+            self.logger.error("Unable to process query. StatusCode: %d." % response.status_code)
+        return None
